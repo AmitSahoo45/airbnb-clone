aa9ef23f56f0817a398c57f384eff7c6f54e04f3
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-09-21 00:50:51 +0530

    Checking Auth Prod. - Ch:01

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -61,4 +61,7 @@ export const authOptions: AuthOptions = {
     }
 }
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }

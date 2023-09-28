65e444a7b155c56e8327acf99a75f4dee6635589
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-09-28 12:06:58 +0530

    Checking Auth Prod. - Ch:02

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com']
-    }
+    },
+    experimental: {
+        appDir: true,
+    },
 }
 
 module.exports = nextConfig
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -61,7 +61,4 @@ export const authOptions: AuthOptions = {
     }
 }
 
-// export default NextAuth(authOptions);
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }
+export default NextAuth(authOptions);
